df8e52fef4b17e7b3b395f2e1044eb18a2e0f637
Anonymous-Hex <102014381+Anonymous-Hex@users.noreply.github.com>
2022-06-28 13:34:25 +0800

    Update main.py
    added shop, more characters and items

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import time
 import sys
 
 coins = 0
+level = 0
 
-#shop
-def changecharacter(): 
+#shops
+
+def changecharacter1(): 
     global coins
     os.system('clear')
+    print("Level:",level)
     print("coins: $", coins)
     chosen = int(input("you reach a tavern and you can change your character [name, shield, health, damage, heal] \n 1 = $500 Jack, 0, 100, 20, 10 \n 2 = $500 Billy, 0, 100, 10, 15 \n 3= $500 Wizas, 0, 75, 15, 20 \n or exit = 0 \n"))
 
@@ -17,11 +20,12 @@ def changecharacter():
         if coins < 500:
             print("not enough coins")
             time.sleep(1)
-            changecharacter()
+            changecharacter1()
         else:
             print("")
             delay_print("you chose Jack")
             player = Player("Jack", 0, 100, 20, 10)
+            character = "Jack"
             coins -= 500
             print("")
             print("coins: $", coins)
@@ -33,11 +37,12 @@ def changecharacter():
         if coins < 500:
             print("not enough coins")
             time.sleep(1)
-            changecharacter()
+            changecharacter1()
         else:
             print("")
             delay_print("you chose Billy")
             player = Player("Billy", 0, 100, 10, 15)
+            character = "Billy"
             coins -= 500
             print("")
             print("coins: $", coins)
@@ -47,11 +52,12 @@ def changecharacter():
         if coins < 500:
             print("not enough coins")
             time.sleep(1)
-            changecharacter()           
+            changecharacter1()           
         else:
             print("")
             delay_print("you chose Wizas")
             player = Player("Wizas", 0, 75, 15, 20)
+            character = "Wizas"
             coins -= 500
             print("")
             print("coins: $", coins)
@@ -64,8 +70,74 @@ def changecharacter():
     else:
         print("invalid input")
         time.sleep(1)
-        changecharacter()
+        changecharacter1()
     os.system('clear')
+
+def changecharacter2():
+    global coins
+    os.system('clear')
+    print("Level:",level)
+    print("coins: $", coins)
+    chosen = int(input("you reach a tavern and you can change your character [name, shield, health, damage, heal] \n 1 = $1000 James, 0, 120, 30, 12 \n 2 = $1000 Bruno, 0, 140, 10, 25 \n 3= $1000 William, 0, 100, 20, 20 \n or exit (the shop) = 0 \n"))
+
+    if chosen == 1:
+        if coins < 1000:
+            print("not enough coins")
+            time.sleep(1)
+            changecharacter2()
+        else:
+            print("")
+            delay_print("you chose James")
+            player = Player("James", 0, 120, 30, 11)
+            character = "James"
+            coins -= 1000
+            print("")
+            print("coins: $", coins)
+            time.sleep(2)
+        
+            
+
+    elif chosen == 2:
+        if coins < 1000:
+            print("not enough coins")
+            time.sleep(1)
+            changecharacter2()
+        else:
+            print("")
+            delay_print("you chose Bruno")
+            player = Player("Bruno", 0, 140, 10, 25)
+            character = "Bruno"
+            coins -= 1000
+            print("")
+            print("coins: $", coins)
+            time.sleep(2)
+
+    elif chosen ==  3:
+        if coins < 1000:
+            print("not enough coins")
+            time.sleep(1)
+            changecharacter2()           
+        else:
+            print("")
+            delay_print("you chose William")
+            player = Player("William", 0, 100, 20, 20)
+            character = "William"
+            coins -= 1000
+            print("")
+            print("coins: $", coins)
+            time.sleep(2)
+    
+    elif chosen == 0:
+        print("exited")
+        time.sleep(3)
+    
+    else:
+        print("invalid input")
+        time.sleep(1)
+        changecharacter2()
+    os.system('clear')    
+
+
 
 def delay_print(s):
     for c in s:
@@ -130,14 +202,15 @@ def battle(Player, Opponent):
         delay_print ("You have been defeated, the game is over")
         print("")
         retry = input("retry (Y/N)?")
-        if retry == "Y":
+        if retry.upper() == "Y":
             battle(Player, Opponent)
-        elif retry == "N":
+        elif retry.upper() == "N":
             exit
     elif Opponent.health <= 0:
         print("")
         delay_print ("You have won the match, moving on...")
         coins =+ 500
+        level =+ 0.5
     elif Opponent.health and Player.health <= 0:
         print("")
         delay_print ("It's a draw, you have been defeated, the game is over")
@@ -211,4 +284,67 @@ elif firstDir == 10:
 print("")
 delay_print("CONGRATULATIONS on completing your first battle")
 
-changecharacter()
+changecharacter1()
+
+secondDir = int(input("You leave the tavern and reach a fork in the road, the left leads to a cave and the right leads to a river \n 1 = left \n 2 = right \n"))
+
+if secondDir == 1:
+    os.system('clear')
+    delay_print ("You turn left into the cave")
+    print("")
+    item = int(input("What item do you pick up? \n 1 = torch (increses health) \n 2 = pickaxe (increses damnage) \n"))
+    if item == 1:
+        delay_print ("you chose the torch...")
+        tool = "torch"
+        Player.health =+ 20
+    
+    if item == 2:
+        delay_print ("you chose the picaxe...")
+        tool = "picaxe"
+        Player.damage =+ 10
+    time.sleep(3)
+    os.system('clear')
+    delay_print ("you walk further down the cave when....")
+    print("")
+    print("A ROCK GOLEM RISES FROM THE GROUD AND GRABS YOUR FOOT!!!")
+    opponent = Opponent("Rock_Golem", 150, 200, 25, 20)
+
+    battle(player, opponent)
+    os.system('clear')
+    delay_print ("CONGRADULATIONS ON defeating the rock golem!!")
+   
+elif secondDir == 2:
+    os.system('clear')
+    delay_print ("You turn right towards the river")
+    print("")
+    item = int(input("What item do you pick up? \n 1 = water proof jacket (increses health) \n 2 = spear (increses damnage) \n"))
+    if item == 1:
+        delay_print ("you chose the water proof jacket...")
+        tool = "water proof jacket"
+        Player.health =+ 20
+    
+    if item == 2:
+        delay_print ("you chose the spear...")
+        tool = "spear"
+        Player.damage =+ 10
+    time.sleep(3)
+    os.system('clear')
+    delay_print("you walk towards the river when....")
+    print("")
+    print("SUDDENLY A SEA SERPENT comes out of the river and begins to fire boiling water at you")
+    opponent = Opponent("Sea_Serpent", 100, 80, 35, 10)
+
+    battle(player, opponent)
+    
+
+    os.system('clear')
+    delay_print ("CONGRADULATIONS ON defeating the Sea Serpent!!")
+
+coins =+ 500
+
+
+
+
+
+    
+    
